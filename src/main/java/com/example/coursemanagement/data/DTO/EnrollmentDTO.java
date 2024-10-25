420db8e323e32605848a7b73322c4cb6dd3a0a56
Truong Tuan Dung <tuandungtruong122@gmai.com>
2024-10-25 18:56:56 +0700

    BESP01004_Commit01_Dũng: Create API Enrollment

---
+++ src/main/java/com/example/coursemanagement/data/DTO/EnrollmentDTO.java
@@ -0,0 +1,31 @@
+package com.example.coursemanagement.data.DTO;
+
+
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EnrollmentDTO {
+    private Integer enrollmentId;
+    private Integer userId; // Sử dụng ID thay vì entity để DTO gọn nhẹ hơn
+    private Integer courseId;
+    private EnrollmentStatus status;
+    private LocalDateTime enrollmentDate;
+    private PaymentStatus paymentStatus;
+
+    public enum EnrollmentStatus {
+        completed,
+        in_progress
+    }
+
+    public enum PaymentStatus {
+        pending,
+        completed,
+        failed
+    }
+}
---
+++ src/main/java/com/example/coursemanagement/data/Enums/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.example.coursemanagement.data.Enums;
+
+public enum PaymentStatus {
+    pending,
+    completed,
+    failed
+}
