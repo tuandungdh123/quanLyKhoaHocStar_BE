f233f21ea1dd5447b3a3f800f4229ed2b8e96d9f
Truong Tuan Dung <tuandungtruong122@gmai.com>
2024-10-28 00:08:22 +0700

    BESP01011_Commit03_Dung: Fix bug getEnrollmentbyCourseId

--- src/main/java/com/example/coursemanagement/service/EnrollmentService.java
+++ src/main/java/com/example/coursemanagement/service/EnrollmentService.java
@@ -3,7 +3,6 @@ package com.example.coursemanagement.service;
 import com.example.coursemanagement.data.DTO.EnrollmentDTO;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EnrollmentService {
     List<EnrollmentDTO> getAllEnrollments();
@@ -12,7 +11,7 @@ public interface EnrollmentService {
 
     EnrollmentDTO getEnrollmentById(Integer enrollmentId);
 
-    Optional<EnrollmentDTO> getEnrollmentsByCourseId(Integer courseId);
+    List<EnrollmentDTO> getEnrollmentsByCourseId(Integer courseId);
 
     EnrollmentDTO addEnrollment(EnrollmentDTO enrollmentDTO);
 }
