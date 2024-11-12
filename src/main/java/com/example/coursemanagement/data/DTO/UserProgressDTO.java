e4cfadf71532a07fac3547562a94191fae827380
Trần Phúc Hậu <tranphuchau3001@gmail.com>
2024-11-12 21:50:42 +0700

    BESP02007_Commit02_Hau: Update API UserProgress

--- src/main/java/com/example/coursemanagement/data/DTO/UserProgressDTO.java
+++ src/main/java/com/example/coursemanagement/data/DTO/UserProgressDTO.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Data
@@ -16,7 +15,7 @@ public class UserProgressDTO {
     private Integer progressId;
     private Integer userId;
     private Integer courseId;
-    private String completedLessons; // JSON lưu danh sách lesson_id đã hoàn thành
-    private Timestamp lastAccessed = Timestamp.valueOf(LocalDateTime.now());;
-    private Double progressPercentage = 0.0;
+    private Integer lessonId;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private String status;
 }
